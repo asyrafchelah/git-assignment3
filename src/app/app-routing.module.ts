aa2fa7127e6e9602c94f5815352f9383d0449754
asyraf_chelah <asyraf_chelah@dell.com>
2019-09-15 16:31:44 +0800

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { UserpageComponent } from './userpage/userpage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: './user/NazmiFaiz', component: UserpageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
